c426b5eec43cc46e16609a10bbfb3fd035726e71
Madhushree2000 <97351016+Madhushree2000@users.noreply.github.com>
2025-05-20 17:15:29 +0200

    validation with traj 04

--- modules/eval/evaluatetraj04.py
+++ modules/eval/evaluatetraj04.py
@@ -20,7 +20,7 @@ from modules.xfeat import XFeat
 
 # Import the CustomDataset from our adapted code - make sure this path is correct
 # You can either merge this file with the previous one or import it properly
-from custom_dataset_pose_benchmark import CustomDataset, compute_pose_error, tensor2bgr
+from modules.eval.batcheddatasettraj_04 import CustomDataset, compute_pose_error, tensor2bgr
 
 
 def parse_args():
