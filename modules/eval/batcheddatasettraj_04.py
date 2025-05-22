ac2731bfa26d54a43ca41191ad1526f876a02089
Madhushree2000 <97351016+Madhushree2000@users.noreply.github.com>
2025-05-23 00:40:00 +0200

    added cuda for eval

--- modules/eval/batcheddatasettraj_04.py
+++ modules/eval/batcheddatasettraj_04.py
@@ -329,6 +329,7 @@ def parse_args():
 
 
 if __name__ == '__main__':
+
     args = parse_args()
     
     # Validate JSON file structure
--- modules/eval/evaluate_traj04.py
+++ modules/eval/evaluate_traj04.py
@@ -489,4 +489,6 @@ def main():
 
 
 if __name__ == "__main__":
+    import torch.multiprocessing as mp
+    mp.set_start_method('spawn', force=True)
     main()
